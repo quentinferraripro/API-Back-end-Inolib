90ccba5649c4747aaef939b1fcdbec8ae00a627f
Matthieu MEIGNAN <matthieu.meignan@inolib.com>
2023-04-18 10:31:43 +0200

    fix: GraphQL schema

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -17,7 +17,7 @@ export const typeDefs = [
         email: String!
         phone: String!
         message: String!
-      ): ContactRequest!
+      ): ContactRequest
     }
 
     type ContactCategory {
